55a6c5a48063c8058f885d0943f2420e874b4de0
tobbrau <tobiasjbraun@gmail.com>
2019-02-06 10:00:21 -0800

    Still need to make crossover function.

--- Enviorment.py
+++ Enviorment.py
@@ -21,26 +21,28 @@ class Enviorment():
         return self.speciate(new)
 
     def generation(self):
-        species_new = []
+        species_champ = []
         species_surv = []
         averages = []
         # Raw scoring
-        for s in self.species:
+        for s in self.species: # TODO add method to update score of each genome
             for g in s:
                 g.score = (g.score * random.gauss(1, self.randomness))/len(s)
-            s.sort(key=Genome.get_score())
-            species_new.append(s[:self.carry])
+            s.sort(key=Genome.get_score(), reverse=True)
+            species_champ.append(s[:self.carry])
             species_surv.append(s[:int(self.top * len(s))])
             averages.append(sum(g.score for g in s))
-
-        self.species = species_new
         # Allocating and creating offspring
+        new_population = []
         total = sum(averages)
-        new_genome_nr = sum(len(s) for s in self.species) - (self.carry * len(self.species))
-        for a, s in zip(averages, species_surv):
-            allocated = round((a/total) * new_genome_nr)
-            new_genome = self._repop(s, allocated)
-            self.speciate(new_genome)
+        new_genome_nr = sum(len(s) for s in self.species)
+        for i in range(len(species_surv)):
+            allocated = round((averages[i]/total) * new_genome_nr)
+            # if allocated is less than 1 then the species does not get added to the next generation
+            new_genome = self._repop(species_surv[i], allocated - len(species_champ[i]))
+            new_population += (species_champ[:allocated][i] + new_genome)
+        self.species = []
+        self.speciate(new_population) # TODO reset innovation tracker
         return self.species
 
     def speciate(self, new):
@@ -56,7 +58,7 @@ class Enviorment():
                 self.species.append([g])
         return self.species
 
-    def distance(self, g1, g2, c1=1, c2=1): # TODO implement proper handeling of c1 and c2
+    def distance(self, g1, g2, c1=1, c2=1): # TODO implement proper handling of c1 and c2
         weights = []
         exess_disjoint = 0
         for gene1 in g1.genes:
@@ -71,12 +73,20 @@ class Enviorment():
         return c1 * (exess_disjoint/max(len(g1.genes), len(g2.genes))) + c2 * (sum(weights)/len(weights))
 
     def _repop(self, s, nr):
+        if nr < 1:
+            return []
+        genome = []
+        for i in range(nr):
+            genome.append(self.crossover(random.choice(s), random.choice(s)))
+        return genome
+
+    def crossover(self, g1, g2):
+        fit = sorted([g1, g2], key=Genome.get_score())
+        fit_ratio = fit[0]/fit[1]
+
         return None
 
-    def _score_species(self, s):
-        return None
-
-    def _value(self):
+    def _value(self, g):
         return 0
 
     def get_innovation(self, in_node, out_node):
